02ae14855488de2e91c2959d26b2e9d83821dce8
Rhys Jones <91537105+RhysJones1@users.noreply.github.com>
2022-02-18 16:05:19 +0000

    Update Intro_To_SQL.sql

--- Introduction/Intro_To_SQL.sql
+++ Introduction/Intro_To_SQL.sql
@@ -1,7 +1,7 @@
-USE sql_store;			/* USE statement allows us to point to a particular database */
+USE sql_store;			      /* USE statement allows us to point to a particular database */
 
-SELECT * 				/* SELECT Allows ut select the columns we want to retrieve. '*' is access all */ 
-FROM customers			/* FROM Allows us to point to a particular table within thr DB */
+SELECT * 				          /* SELECT Allows ut select the columns we want to retrieve. '*' is access all */ 
+FROM customers			      /* FROM Allows us to point to a particular table within thr DB */
 -- WHERE customer_id = 1	/* WHERE allows us to filter the result */
-ORDER BY first_name			/* ORDER BY allows us to order our data by a certain column */
-/* While the above statements are optional, when we do use them, you cannot change the order of them */
+ORDER BY first_name			  /* ORDER BY allows us to order our data by a certain column */
+                          /* While the above statements are optional, when we do use them, you cannot change the order of them */
