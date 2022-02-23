d5ec6cad9df3679b66a4a4d47f31fc90e724aa86
Rhys Jones <91537105+RhysJones1@users.noreply.github.com>
2022-02-23 11:38:02 +0000

    Delete 2_Retrieving_Data_From_MultipleTables directory

--- 2_Retrieving_Data_From_MultipleTables/ExploringINNER_JOINS_Exercise.sql
+++
@@ -1,5 +0,0 @@
-SELECT order_id, order_items.product_id, quantity, order_items.unit_price
-FROM order_items 
-JOIN products 
-	ON order_items.product_id = products.product_id
-	
--- 2_Retrieving_Data_From_MultipleTables/ExploringJoining_Databases.sql
+++
@@ -1,8 +0,0 @@
--- Joining databases
--- You only need to prefix the tables that are not part of the current database
--- for example, here the products table belongs to a different table, therefore we prefix it with sql_inventory 
--- which is the name of the database
-SELECT *
-FROM order_items 
-JOIN sql_inventory.products
-	ON order_items.product_id = products.product_id
